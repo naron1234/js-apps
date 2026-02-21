52a31e93493e03d31055a78efc4e4a96af3537f7
naron1234 <bb4gjcf7nr@privaterelay.appleid.com>
2026-02-21 12:41:18 +0100

    to do list

--- toDo App/main.js
+++ toDo App/main.js
@@ -5,6 +5,8 @@ const taskCount = document.getElementById('taskCount');
 
 let tasks = [];
 
+loadTasks();
+
 addBtn.addEventListener('click', () => {
     const taskText = taskInput.value.trim();
     
@@ -15,6 +17,63 @@ addBtn.addEventListener('click', () => {
     taskInput.value = '';
 
     console.log(tasks);
+
+    saveTasks();
+    renderTasks();
+
+
     
-})
+});
+
+function renderTasks() {
+    taskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+
+    const li = document.createElement('li');
+
+    li.className = 'task-group-item d-flex justify-content-between align-items-center';
+
+    li.innerHTML = `
+        <span>${task}</span>
+        <button class="btn btn-danger btn-sm delete-btn">Delete</button>
+    `;
+   
+    li.querySelector('button').addEventListener('click', () => {
+    deleteTask(index);
+
+ });
+
+    taskList.appendChild(li);
+
+
+
+
+
+ });
+
+   taskCount.textContent = tasks.length;
+
+
+ 
+
+}    
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
+    saveTasks();
+}
+
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+        tasks = JSON.parse(storedTasks);
+        renderTasks();
+    }
+
+}
        
